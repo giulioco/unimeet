ec90624ff9cd9acc0fb2c660a2574d762f77f0d9
Giulio Colleluori <gcollelu@ucsc.edu>
2018-02-03 14:53:07 -0800

    sprint review added [still missing burnup chart] + database schema for users, activities and matches created

--- UniMeetApp/db/schema.rb
+++ UniMeetApp/db/schema.rb
@@ -15,6 +15,25 @@ ActiveRecord::Schema.define(version: 20180201062400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "matches", force: :cascade do |t|
+    t.references "user", null:false
+    t.references "activity", null:false
+  end
+
+  create_table "activities", force: :cascade do |t|
+    t.references "project_owner", null:false
+    t.string "name", null:false
+    t.text "description", null:false
+    t.datetime "recruitment_deadline"
+    t.integer "max_size"
+    t.string "activity_id"
+    t.text "activity_logo_url"
+    t.string "requirements", array:true, default: []
+    t.references "members", array:true, default: []
+    
+    t.index ["activity_id"], name: "index_activities_on_activity_id", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -28,6 +47,13 @@ ActiveRecord::Schema.define(version: 20180201062400) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", default: "", null: false
+    t.string "profile_pic_url", default: "", null: false
+    t.text "bio", default: "", null: false
+    t.string "college_location", default: "", null: false
+    t.string "interests", array:true, default:[]
+    t.references "matches", array:true, default:[]
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
