e729e7402fee50e780244ae782eb34001b3ae673
Erik Haight <edhaight@ucsc.edu>
2018-02-14 08:51:14 -0800

    Removed extra users directory
    Deleted extra users directory in views folder. Was accidentally made when I was implementing the mailer.

--- UniMeetApp/db/schema.rb
+++ UniMeetApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205201317) do
+ActiveRecord::Schema.define(version: 20180212190827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20180205201317) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_owner_id"], name: "index_activities_on_project_owner_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.string "like_id", null: false
+    t.string "user_id", null: false
+    t.string "liked_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "matches", force: :cascade do |t|
