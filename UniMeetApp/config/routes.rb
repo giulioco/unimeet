e1226da9eab5b8df82ee58c2d9c9e28cb17f64e1
Erik Haight <edhaight@ucsc.edu>
2018-02-14 10:52:03 -0800

    Created users controller
    Created users controller and accordingly created html files for edit show and update user.

--- UniMeetApp/config/routes.rb
+++ UniMeetApp/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/update'
+
+  get 'users/edit'
+
+  get 'users/show'
+
   devise_for :users
 	root 'pages#home'
+	resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
