ee25a8ae43414b8b2d437d6108b9f37671057f82
Erik Haight <edhaight@ucsc.edu>
2018-02-18 17:35:22 -0800

    Profile picture
    Implemented profile picture functionality for the user class. Added according columns to the users table and used the paperclip gem. Thumb icon and medium icon are available. Other models can be customized as well. Users can upload/change their profile picture.   I put the thumb icon on the drop down menu for the user, and styled it briefly to give it a round border. This will need some minor tweaking on the frontend as it slightly distorts rectangular images when converting to a circular one. If the user does not have an image yet, the drop down menu button just displays "My Account".

--- UniMeetApp/app/helpers/users_helper.rb
+++ UniMeetApp/app/helpers/users_helper.rb
@@ -6,9 +6,23 @@ module UsersHelper
       @user.first_name + " " + @user.middle_name + " " + @user.last_name
     end
   end
+  
   def age
     if (@user.birthday != nil)
       (Date.today.strftime('%Y%m%d').to_i - @user.birthday.strftime('%Y%m%d').to_i) / 10000
+    end
+  end
+
+  def display_image(size)
+    if(current_user.image_file_size != nil)
+      image_tag(current_user.image.url(size))
+    end
+  end
+  
+  def display_image_menu(size)
+    if(current_user.image_file_size != nil)
+      image_tag(current_user.image.url(size), :class => 'round-image-50') 
+    else "My Account"
     end
   end
 end
--- UniMeetApp/db/schema.rb
+++ UniMeetApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216013348) do
+ActiveRecord::Schema.define(version: 20180218235455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,10 @@ ActiveRecord::Schema.define(version: 20180216013348) do
     t.text "bio", default: ""
     t.string "college_location", default: ""
     t.date "birthday"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
