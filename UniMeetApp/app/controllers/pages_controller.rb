24637870276bd9a7849b363c39882ec2ccb11051
Erik Haight <edhaight@ucsc.edu>
2018-02-19 20:52:47 -0800

    Fixed merge bugs

--- UniMeetApp/app/controllers/pages_controller.rb
+++ UniMeetApp/app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
 	before_action :authenticate_user! 
 	def home
-		@user = User.find(params[:id])
 		@this_user = current_user
 		@activity = Activity.find(6)
 	end
