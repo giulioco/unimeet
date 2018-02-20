451c0e8599ecd0d59901118ca11989e0fced15bf
Erik Haight <edhaight@ucsc.edu>
2018-02-19 16:35:41 -0800

    Removed profile pic url column
    Removed profile pic url column from users table.

--- UniMeetApp/db/schema.rb
+++ UniMeetApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180218235455) do
+ActiveRecord::Schema.define(version: 20180220002650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,7 +71,6 @@ ActiveRecord::Schema.define(version: 20180218235455) do
     t.string "first_name", null: false
     t.string "middle_name", default: ""
     t.string "last_name", null: false
-    t.string "profile_pic_url", default: ""
     t.text "bio", default: ""
     t.string "college_location", default: ""
     t.date "birthday"
