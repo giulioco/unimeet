1498e76ea97816aa2e1fdf8a3789bdedab2b0c04
Erik Haight <edhaight@ucsc.edu>
2018-02-20 15:46:14 -0800

    Default Pictures

--- UniMeetApp/app/helpers/activities_helper.rb
+++ UniMeetApp/app/helpers/activities_helper.rb
@@ -1,7 +1,5 @@
 module ActivitiesHelper
-	def display_activity_image(size)
-	    if(@activity.image_file_size != nil)
-	      image_tag(@activity.image.url(size))
+	def display_image(size)
+      image_tag(@user.image.url(size))
     end
-  end
 end
--- UniMeetApp/app/models/activity.rb
+++ UniMeetApp/app/models/activity.rb
@@ -6,7 +6,7 @@ class Activity < ActiveRecord::Base
   #validates :name, :max_size => true
   validates :name, :description, :max_size, :presence => true
 
-  has_attached_file :image, styles: { medium: "300x300#", thumb: "50x50#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "300x300#", thumb: "50x50#" }, default_url: ":style/missing_avatar.png"
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
   #has_many :members
 end
