691200d64f57f850f87676ca8ea60ac5c12a9198
Erik Haight <edhaight@ucsc.edu>
2018-02-19 16:59:00 -0800

    Add major column to users table
    Added major column to users table and made according fields for major on
    the edit and show pages.

--- UniMeetApp/db/schema.rb
+++ UniMeetApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220002650) do
+ActiveRecord::Schema.define(version: 20180220004513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20180220002650) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.string "major"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
