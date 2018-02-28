5bcffc33fe6f04162700dec4c7dc15fca5a424cc
Erik Haight <edhaight@ucsc.edu>
2018-02-27 21:23:38 -0800

    Added Validation of Activity Description
    Added validation for activity description to be under 700 characters.

--- UniMeetApp/app/models/activity.rb
+++ UniMeetApp/app/models/activity.rb
@@ -6,6 +6,7 @@ class Activity < ActiveRecord::Base
   #attr_accessible :name, :description, :max_size
   #validates :name, :max_size => true
   validates :name, :description, :max_size, :presence => true
+  validates_length_of :description, :maximum => 700
 
   has_attached_file :image, styles: { medium: "300x300#", thumb: "50x50#" }, default_url: ":style/missing_avatar.png"
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
