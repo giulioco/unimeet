e0cbe27fee40b79a4eec118fdfb4f05085452031
Giulio Colleluori <gcollelu@ucsc.edu>
2018-03-03 17:23:55 -0800

    fixed modal in match animation

--- UniMeetApp/app/assets/javascripts/modal.js
+++ UniMeetApp/app/assets/javascripts/modal.js
@@ -1,7 +1,7 @@
 
-$('#myModal').on('show.bs.modal', function (e) {
+$('#myModal').on('show.bs.modal', function () {
       $('.modal .modal-dialog').attr('class', 'modal-dialog tada animated');
-})
-$('#myModal').on('hide.bs.modal', function (e) {
+});
+$('#myModal').on('hide.bs.modal', function () {
       $('.modal .modal-dialog').attr('class', 'modal-dialog lightSpeedOut animated');
-})
+});
