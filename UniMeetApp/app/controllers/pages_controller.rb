5acff9df0ed3bc7461dfca4c998c9cdfbb6686fc
Giulio Colleluori <gcollelu@ucsc.edu>
2018-03-03 18:26:13 -0800

    implemented logic to handle edge case in which there is no activities or users left in the queue for both user and activity perspective, regular and partial rendering

--- UniMeetApp/app/controllers/pages_controller.rb
+++ UniMeetApp/app/controllers/pages_controller.rb
@@ -51,16 +51,23 @@ class PagesController < ApplicationController
     @all = Activity.all
     @notWanted = Activity.where(id: Activity.joins(:memberships).where({ "memberships.user_id" => current_user.id})).or(Activity.where(id: Activity.joins(:likes).where({"likes.user_id" => current_user.id}).where.not("likes.user_likes_activity" => nil)))
     @queue = @all - @notWanted
-    @oldActivity = @activity
-    @activity = @queue.first()
-    if @itsMatch
-      respond_to do |format|           
-        format.js { render :action => "match_animation" }
+    if @queue.count > 0 
+      @oldActivity = @activity
+      @activity = @queue.first()
+      if @itsMatch
+        respond_to do |format|           
+          format.js { render :action => "match_animation" }
+        end
       end
-    end
-    respond_to do |format|               
-      format.js
-    end
+      respond_to do |format|               
+        format.js
+      end
+    else 
+      @type = 'activity'
+      respond_to do |format|           
+        format.js { render :action => "empty_deck" }
+      end
+    end 
   end
 
   def dislike_activity
@@ -70,10 +77,18 @@ class PagesController < ApplicationController
   @all = Activity.all
   @notWanted = Activity.where(id: Activity.joins(:memberships).where({ "memberships.user_id" => current_user.id})).or(Activity.where(id: Activity.joins(:likes).where({"likes.user_id" => current_user.id}).where.not("likes.user_likes_activity" => nil)))
   @queue = @all - @notWanted
-  @activity = @queue.first()
-  respond_to do |format|               
-      format.js 
+  if @queue.count > 0 
+    @activity = @queue.first()
+    respond_to do |format|               
+        format.js 
     end
+  else 
+      @type = 'activity'
+      respond_to do |format|           
+        format.js { render :action => "empty_deck" }
+      end
+  end 
+
   end
 
   def like_profile
@@ -86,14 +101,21 @@ class PagesController < ApplicationController
     @oldUser = @user
     @user = @queue.first()
     puts @user.id 
-    if @itsMatch
-      respond_to do |format|           
-        format.js { render :action => "match_animation_activity" }
+    if @queue.count > 0 
+      if @itsMatch
+        respond_to do |format|           
+          format.js { render :action => "match_animation_activity" }
+        end
       end
-    end
-    respond_to do |format|               
-      format.js 
-    end
+      respond_to do |format|               
+        format.js 
+      end
+    else 
+      @type = 'user'
+      respond_to do |format|           
+        format.js { render :action => "empty_deck" }
+      end
+    end 
   end
 
   def dislike_profile
@@ -101,13 +123,20 @@ class PagesController < ApplicationController
     @activity = Activity.find(session[:current_activity_id])
     @like = @activity.likes.find_by_activity_id(params[:id])
     @user.dislike_profile!(@user, @activity)
-    all = User.all
-    notWanted = User.where(id: current_user.id).or(User.where(id: User.joins(:likes).where({"likes.activity_id" => session[:current_activity_id]}).where.not("likes.activity_likes_user" => nil))) 
-    queue = all - notWanted 
-    nextUserInQueue = queue.first() 
-    respond_to do |format|               
-      format.js 
-    end
+    @all = User.all
+    @notWanted = User.where(id: current_user.id).or(User.where(id: User.joins(:likes).where({"likes.activity_id" => session[:current_activity_id]}).where.not("likes.activity_likes_user" => nil))) 
+    @queue = @all - @notWanted 
+    if @queue.count > 0
+      @nextUserInQueue = @queue.first() 
+      respond_to do |format|               
+        format.js 
+      end
+    else 
+      @type = 'user'
+      respond_to do |format|           
+        format.js { render :action => "empty_deck" }
+      end
+    end 
   end
 
   def unmatch_activity
