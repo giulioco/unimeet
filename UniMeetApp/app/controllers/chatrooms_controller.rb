9b8fec016a3b2871f17c4537ce09e98efbef107d
Giulio Colleluori <gcollelu@ucsc.edu>
2018-03-10 00:23:44 -0800

    fixed manual chat

--- UniMeetApp/app/controllers/chatrooms_controller.rb
+++ UniMeetApp/app/controllers/chatrooms_controller.rb
@@ -66,11 +66,7 @@ class ChatroomsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_chatroom
-      if id == nil
-        @chatroom = chatroom
-      else
-        @chatroom = Chatroom.find(params[:id])
-      end
+      @chatroom = Chatroom.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
