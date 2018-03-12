7aac551cabfa6d274ad919ffe4c4cf22b697d424
Giulio Colleluori <gcollelu@ucsc.edu>
2018-03-12 13:15:29 -0700

    trying to deploy chat

--- UniMeetApp/config/development.rb
+++ UniMeetApp/config/development.rb
@@ -1,4 +1,6 @@
 # config/development.rb
 Rails.application.configure do 
   config.action_cable.url = "ws://localhost:3000/cable"
+  #config.action_cable.url = 'ws://unimeetapp-development.ckfbsrxa15vu.us-west-1.rds.amazonaws.com/cable'
+  #config.action_cable.allowed_request_origins = ['http://localhost:3001']
 end 
